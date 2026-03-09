63cb53a59d188325531fc13fd7979f396cb7ef41
Nicholas Miazzo <nmiazzo1211@icloud.com>
2026-03-09 12:00:42 +0100

    Force 13 char EAN in product page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRippleModule } from '@angular/material/core';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'pw-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule, MatRippleModule],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule, MatRippleModule, MatSnackBarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
-export class App {}
+export class App implements OnInit, OnDestroy {
+  private swUpdate = inject(SwUpdate);
+  private snackBar = inject(MatSnackBar);
+  private updateSub: Subscription | null = null;
+
+  ngOnInit(): void {
+    if (!this.swUpdate.isEnabled) return;
+
+    this.updateSub = this.swUpdate.versionUpdates
+      .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+      .subscribe(() => {
+        const ref = this.snackBar.open('Nuova versione disponibile', 'Aggiorna', { duration: 0 });
+        ref.onAction().subscribe(() => document.location.reload());
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.updateSub?.unsubscribe();
+  }
+}
